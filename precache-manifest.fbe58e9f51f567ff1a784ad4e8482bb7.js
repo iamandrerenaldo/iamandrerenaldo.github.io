a52512b35b3ae4e391c660d63a54976ee64156dc
Andre Renaldo <iamandrerenaldo@gmail.com>
2019-10-23 17:56:36 +0800

    Deploy

---
+++ precache-manifest.fbe58e9f51f567ff1a784ad4e8482bb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cde0038e63331b75b0",
+    "url": "/css/app.dd1b9eba.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f23585459db9502f8d33feb25854112c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404003dc4438b1741ae6",
+    "url": "/js/about.fa297504.js"
+  },
+  {
+    "revision": "59cde0038e63331b75b0",
+    "url": "/js/app.4a709aeb.js"
+  },
+  {
+    "revision": "3e9e93db744c65429f8d",
+    "url": "/js/chunk-vendors.3eb09af2.js"
+  },
+  {
+    "revision": "43d1b15e8845ef6403142928029793c3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
